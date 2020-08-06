d8a4a41dcd339a4c4515c56b68f1ece5b308195c
Fleetwood <wizening@gmail.com>
2020-08-06 03:22:05 -0400

    Updated env.

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 const google = require('./google.json');
 const server = {
-    host: process.env.HOST || 'localhost'
+    host: 'localhost'
     , port: process.env.PORT || 8080
-    , env: process.env.env || 'development'
+    , env: process.env.env || 'stage'
 }
 const pg = {
     host: process.env.DB_HOST
